fba173f46f6e7c82665da84873437dabb489212a
Fredy Orellana <forellanaf@tigo.com.gt>
2019-09-12 19:22:23 -0600

    Update to deploy to heroku

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 // Instalar el servidor express
-const express = require ('express');
-const path = require ('ruta');
-
-aplicación const = express ();
+const path = require('path');
+const express = require('express');
+const app = express();
 
 // Servir solo los archivos estáticos del directorio dist
-app.use (express.static (__ dirname + '/ dist / <name-of-app>'));
+app.use(express.static(__dirname + '/dist/angularbases'));
 
-app.get ('/ *', function (req, res) {
-    
-res.sendFile (path.join (__ dirname + '/ dist / <name-of-app> /index.html'));
+app.get('/ *', function(req, res) {
+
+    res.sendFile(path.join(__dirname + '/dist/angularbases/index.html'));
 });
 
 // Inicie la aplicación escuchando en el puerto predeterminado de Heroku
-app.listen (process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080);
--- src/app/server.js
+++ src/app/server.js
@@ -1,16 +1,12 @@
 // Instalar el servidor express
-const express = require('express');
-const path = require('ruta');
 
-aplicación
-const = express();
 
 // Servir solo los archivos estáticos del directorio dist
-app.use(express.static(__ dirname + '/dist/angularbases'));
+app.use(express.static(__dirname + '/dist/angularbases'));
 
 app.get('/ *', function(req, res) {
 
-    res.sendFile(path.join(__ dirname + '/dist/angularbases/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/angularbases/index.html'));
 });
 
 // Inicie la aplicación escuchando en el puerto predeterminado de Heroku
