d63004382a5fced5079ff1e648b2b2af123cc517
Fredy Orellana <forellanaf@tigo.com.gt>
2019-09-13 11:18:14 -0600

    Update to deploy to heroku

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,20 +1,31 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { HttpHeaders } from '@angular/common/http';
 import { tap } from 'rxjs/operators';
 
 
 @Injectable({
   providedIn: 'root'
 })
+
+
+
 export class DataService {
+  
+   httpOptions = {
+    headers: new HttpHeaders({
+      'Access-Control-Allow-Origin':  '*'
+    })
+  };
+
+
+ 
 
   constructor( private http: HttpClient ) { }
 
 
   getPosts() {
-    return this.http.get('https://jsonplaceholder.typicode.com/posts')
+    return this.http.get('https://jsonplaceholder.typicode.com/posts', {headers: {'Access-Control-Allow-Origin':  '*'}}  )
           .pipe( tap( console.log ) );
   }
-
 }
